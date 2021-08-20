8e43c7429b81915d6709e2a9dd30e7990b1de2db
Ariel Barabas <810664+knoid@users.noreply.github.com>
2021-08-20 19:10:32 -0300

    frog contains next ball inside

--- src/game/Frog.ts
+++ src/game/Frog.ts
@@ -1,28 +1,67 @@
 import AlphaPicture from "./AlphaPicture";
 import { Animation, Orientation } from "./Animation";
+import Picture from "./Picture";
 import Resources from "./Resources";
 import Scene from "./Scene";
 
+class HatchBack extends Picture {
+  public ballInside: Animation;
+  private alphaContext: CanvasRenderingContext2D;
+
+  constructor(resources: Resources) {
+    super(resources, "_hatchBack", 31, 12);
+    this.fill([0, 0, 0]);
+    this.alphaContext = this.context2d;
+
+    this.context2d = resources.getCanvas(this.image.width, this.image.height);
+    this.image = this.context2d.canvas;
+
+    const ballInside = new Animation(
+      resources,
+      "baDotz",
+      47 - 31,
+      21 - 12,
+      6,
+      Orientation.horizontal
+    );
+    ballInside.pivot = [7, 33];
+    this.ballInside = ballInside;
+
+    this.drawBallInside();
+  }
+
+  private drawBallInside() {
+    this.context2d.globalCompositeOperation = "copy"; // replaces everything
+    this.ballInside.draw(this.context2d);
+    this.context2d.globalCompositeOperation = "destination-out";
+    this.context2d.drawImage(this.alphaContext.canvas, 0, 0);
+    super.draw(this.context2d);
+    this.context2d.globalCompositeOperation = "source-over";
+  }
+
+  remove() {
+    super.remove();
+    this.resources.freeCanvas(this.alphaContext);
+  }
+}
 export default class Frog extends Scene {
-  private ballInside: Animation;
+  private hatchBack: HatchBack;
 
   constructor(resources: Resources, x: number, y: number) {
     const image = resources.image("smallFrogOnPad");
     super(x - image.width / 2, y - image.height / 2);
     this.pivot = [image.width / 2, image.height / 2];
 
-    const ballInside = new Animation(
-      resources,
-      "baDotz",
-      47,
-      21,
-      6,
-      Orientation.horizontal
-    );
-    ballInside.pivot = [7, 33];
-    ballInside.show = false;
-    this.ballInside = ballInside;
+    this.hatchBack = new HatchBack(resources);
 
-    this.addActors([new AlphaPicture(resources, "smallFrogOnPad"), ballInside]);
+    this.addActors([
+      new AlphaPicture(resources, "smallFrogOnPad"),
+      this.hatchBack,
+    ]);
+  }
+
+  remove(): void {
+    super.remove();
+    this.hatchBack.remove();
   }
 }
